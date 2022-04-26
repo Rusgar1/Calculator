2318e4819ea210240d9cc1a6a784862215dbcf27
Rusgar1 <87265761+Rusgar1@users.noreply.github.com>
2022-04-26 22:37:48 +0400

    feat: edit Main.java file

--- src/Main.java
+++ src/Main.java
@@ -15,58 +15,59 @@ public class Main {
                 throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             }
 
-            if (expression[1].length() != 1 || !isValidIdentifier(expression[1].toCharArray()[0])){
+            if (expression[1].length() != 1 || !isValidIdentifier(expression[1].toCharArray()[0])) {
                 scanner.close();
                 throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             }
 
-            if (isArabianDigit(expression[0]) & isArabianDigit(expression[2]) & !isRomanDigit(expression[0]) & !isRomanDigit(expression[2])){
-                int a = Integer.parseInt(expression[0]);
-                int b = Integer.parseInt(expression[2]);
-                char operator = expression[1].toCharArray()[0];
-                if (a < 1 | a > 10 | b < 1 | b > 10) {
-                    scanner.close();
-                    throw new Exception("Используются только числа от 1 до 10");
-                }
-
-                System.out.println(Calculator.calculated(a, b, operator));
-            } else if (isRomanDigit(expression[0]) & isRomanDigit(expression[2])){
+            if (isRomanDigit(expression[0]) && isRomanDigit(expression[2])) {
                 int a = Converter.romanToArabic(expression[0]);
                 int b = Converter.romanToArabic(expression[2]);
-                char operator = expression[1].toCharArray()[0];
-                if (a < 1 | a > 10 | b < 1 | b > 10) {
-                    scanner.close();
+                if (a == -1 || b == -1) {
                     throw new Exception("Используются только числа от 1 до 10");
                 }
-
+                char operator = expression[1].toCharArray()[0];
                 int result = (Calculator.calculated(a, b, operator));
-                if (result < 1){
-                    scanner.close();
+                if (isNegativeValue(result)) {
                     throw new Exception("т.к. в римской системе нет отрицательных чисел");
                 }
-
-                String result1 = (Converter.arabicToRoman(Calculator.calculated(a, b, operator)));
-                System.out.println(result1);
-            } else if (!expression[0].matches(".") | !expression[2].matches(".")){
-                scanner.close();
+                System.out.println(Converter.arabicToRoman(result));
+            } else if ((isArabianDigit(expression[0]) && isDotContaining(expression[2]))
+                    || (isDotContaining(expression[0]) && isArabianDigit(expression[2]))) {
                 throw new Exception("Используются только ЦЕЛЫЕ числа от 1 до 10");
+            } else if (isArabianDigit(expression[0]) && isArabianDigit(expression[2])) {
+                int a = Integer.parseInt(expression[0]);
+                int b = Integer.parseInt(expression[2]);
+                if (isValueInDiap(a) && isValueInDiap(b)){
+                    char operator = expression[1].toCharArray()[0];
+                    System.out.println(Calculator.calculated(a, b, operator));
+                } else {
+                    throw new Exception("Используются только числа от 1 до 10");
+                }
             } else {
-                scanner.close();
                 throw new Exception("т.к. используются одновременно разные системы счисления");
             }
         }
     }
 
     static boolean isArabianDigit(String chunk){
-        if (!chunk.matches("\\d+")){
-            return false;
-        }
-        int value = Integer.parseInt(chunk);
-        return value > 0 && value <= 10 ? true : false;
+        return chunk.matches("\\d+");
     }
 
     static boolean isRomanDigit(String chunk){
         return Converter.romanToArabic(chunk) != -1 ? true : false;
+    }
+
+    static boolean isDotContaining(String chunk){
+        return chunk.length() != chunk.replaceAll("\\.", "").length();
+    }
+
+    static boolean isValueInDiap(int value) {
+        return value > 0 && value < 11;
+    }
+
+    static boolean isNegativeValue(int value) { //возвращает если ведичина меньше 0
+        return value < 0;
     }
 
     static boolean isValidIdentifier(char identifier){
